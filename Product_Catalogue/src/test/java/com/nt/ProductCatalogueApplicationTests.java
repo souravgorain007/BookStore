e0475de63dc803e2da3d94ea09a39eef4f4718d5
souravgorain <souravgorain@localhost>
2025-08-17 18:12:08 +0530

    Added FindAll test case.

--- Product_Catalogue/src/test/java/com/nt/ProductCatalogueApplicationTests.java
+++ Product_Catalogue/src/test/java/com/nt/ProductCatalogueApplicationTests.java
@@ -1,9 +1,16 @@
 package com.nt;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.List;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,12 +22,17 @@ import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nt.dto.ProductRequest;
+import com.nt.dto.ProductResponse;
+import com.nt.entity.Product;
+import com.nt.repository.IProductRepository;
 
 @SpringBootTest
 @Testcontainers
 @AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class ProductCatalogueApplicationTests {
 	
 	@Container
@@ -30,6 +42,8 @@ class ProductCatalogueApplicationTests {
 	private MockMvc mockMvc;
 	@Autowired
 	private ObjectMapper objectMapper;
+	@Autowired
+	private IProductRepository productRepository;
 	
 	@DynamicPropertySource
 	public static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
@@ -41,6 +55,7 @@ class ProductCatalogueApplicationTests {
 
 
 	@Test
+	@Order(1)
 	void shouldCreateProduct() throws Exception{
 		ProductRequest productRequest = getProductRequest();
 		
@@ -48,6 +63,9 @@ class ProductCatalogueApplicationTests {
 		       .contentType(MediaType.APPLICATION_JSON)
 		       .content(objectMapper.writeValueAsString(productRequest)))
 		       .andExpect(status().isCreated());
+		
+		List<Product> response = productRepository.findAll();
+		assertThat(response).hasSize(1);
 		
 	}
 	
@@ -57,6 +75,26 @@ class ProductCatalogueApplicationTests {
 				             .description("Apple product.")
 				             .price(52000.0)
 				             .build();
+	}
+	
+	@Test
+	@Order(2)
+    void testFindAllProduct() throws Exception{
+	String jsonResponse = 	mockMvc.perform(get("/product/findAll")
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andReturn()
+				.getResponse()
+				.getContentAsString();
+	
+	List<ProductResponse> responseList = 
+			objectMapper.readValue(jsonResponse, new TypeReference<List<ProductResponse>>() {});
+	
+	assertThat(responseList).hasSize(1);
+	assertThat(responseList).extracting(ProductResponse :: getName)
+	   .containsExactlyInAnyOrder("iPhone 12");
+	
+	
 	}
 
 }
